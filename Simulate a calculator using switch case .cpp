dac93a2633a12893b1cdb589f5b4f7624c9cf771
nkomobruce49-maker <nkomobruce49@gmail.com>
2025-11-19 15:07:22 +0530

    Create Simulate a calculator using switch case .cpp

---
+++ Simulate a calculator using switch case .cpp
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+int main()  
+{ 
+  char op; 
+  float first, second; 
+  printf("Enter an operator (+, -, *, /): "); 
+  scanf("%c", &op); 
+  printf("Enter two operands: "); 
+  scanf("%f %f", &first, &second); 
+  switch (op) { 
+    case '+': 
+      printf("%.1f + %.1f = %.1f", first, second, first + second); 
+      break; 
+    case '-': 
+      printf("%.1f - %.1f = %.1f", first, second, first - second); 
+      break; 
+    case '*': 
+      printf("%.1f * %.1f = %.1f", first, second, first * second); 
+      break; 
+    case '/': 
+      printf("%.1f / %.1f = %.1f", first, second, first / second); 
+      break; 
+    // operator doesn't match any case constant 
+    default: 
+      printf("Error! operator is not correct"); 
+  } 
+  return 0; 
+} 
